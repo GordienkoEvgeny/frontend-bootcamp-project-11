f5fe3a6e4234c5482e014527fe926e18fd1492d7
EvgenyGordienko <evgeny.gordienko95@gmail.com>
2023-01-30 13:18:23 +0300

    add bootstrap

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      }, {
+        test: /\.(scss)$/,
+        use: [{
+          // вставить CSS на страницу
+          loader: 'style-loader'
+        }, {
+          // переводит CSS в модули CommonJS
+          loader: 'css-loader'
+        }, {
+          // Выполнить действия postcss
+          loader: 'postcss-loader',
+          options: {
+            // `postcssOptions` требуется для postcss 8.x;
+            // если Вы используете postcss 7.x пропустите ключ
+            postcssOptions: {
+              // плагины postcss, можно экспортировать в postcss.config.js
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          // компилирует Sass в CSS
+          loader: 'sass-loader'
+        }]
+      }
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
+
+
