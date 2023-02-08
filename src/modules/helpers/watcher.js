77e351d5dc8c2efe4a76854095deae4cc05d725d
GordienkoEvgeny <evgeny.gordienko95@gmail.com>
2023-02-08 12:21:53 +0300

    fixed linter bugs (2)

--- src/modules/helpers/watcher.js
+++ src/modules/helpers/watcher.js
@@ -1,5 +1,6 @@
 import onChange from 'on-change';
 import state from '../appState.js';
+// eslint-disable-next-line import/no-cycle
 import { render, renderFeeds, renderPosts } from '../../view.js';
 
 const watchedState = onChange(state, () => {
