5d6341480e0cfb43e6c4478dc59986b1b007828f
GordienkoEvgeny <evgeny.gordienko95@gmail.com>
2023-02-08 12:05:42 +0300

    fixed linter bugs

--- src/view.js
+++ src/view.js
@@ -1,7 +1,7 @@
 import * as component from './modules/UI.js';
-import { state } from './modules/appState.js';
-import { i18nInstance } from './modules/helpers/i18n.js';
-import { watchedState } from './modules/helpers/watcher.js';
+import state from './modules/appState.js';
+import i18nInstance from './modules/helpers/i18n.js';
+import watchedState from './modules/helpers/watcher.js';
 
 export const render = () => {
   component.inputSubmitForm.reset();
